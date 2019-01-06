8843233d542979bbb16e8b9fd17a6bf5318e8c89
Apeu <jwkim98@kaist.ac.kr>
2019-01-06 14:27:06 +0900

    add: cmake set for default

---
+++ TakiMatrix/TakiMatrix.cpp
@@ -0,0 +1,12 @@
+﻿// TakiMatrix.cpp : Defines the entry point for the application.
+//
+
+#include "TakiMatrix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
