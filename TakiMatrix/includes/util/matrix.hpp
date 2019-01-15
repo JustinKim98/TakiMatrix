aa47f103906c4ce86cb25a650017aa8811ef3b4b
jwkim98 <jwkim98@kaist.ac.kr>
2019-01-15 22:36:21 +0900

    add: compare_matrix functor

--- TakiMatrix/includes/util/matrix.hpp
+++ TakiMatrix/includes/util/matrix.hpp
@@ -4,26 +4,42 @@
 
 #ifndef TAKIMATRIX_MATRIX_HPP
 #define TAKIMATRIX_MATRIX_HPP
-#include <memory>
-#include "../cpu_emulator/processor_util/matrix_object.hpp"
 
-namespace TakiMatrix{
+#include "../cpu_emulator/processor_util/matrix_object.hpp"
+#include <memory>
+
+namespace TakiMatrix {
 
     size_t matrix_id = 0;
 
-    class matrix{
+    class matrix {
     public:
         matrix(const std::vector<float>& data, const std::vector<size_t>& shape);
+
         matrix(matrix& new_matrix);
+
         matrix(matrix&& new_matrix) noexcept;
 
         ~matrix();
+
+        size_t get_id() const;
 
     private:
         std::unique_ptr<processor::matrix_object> m_matrix_ptr;
 
         size_t m_matrix_id;
     };
-}
 
-#endif //TAKIMATRIX_MATRIX_HPP
+    struct compare_matrix {
+        bool operator()(const matrix& first, const matrix& second)
+        {
+            return first.get_id()>second.get_id();
+        }
+    };
+
+    struct matrix_hash_functor {
+        size_t operator()(const matrix& obj) const;
+    };
+} // namespace TakiMatrix
+
+#endif // TAKIMATRIX_MATRIX_HPP
--- TakiMatrix/sources/util/matrix.cpp
+++ TakiMatrix/sources/util/matrix.cpp
@@ -5,7 +5,9 @@
 #include "../../includes/util/matrix.hpp"
 
 namespace TakiMatrix {
-    matrix::matrix(const std::vector<float>& data, const std::vector<size_t>& shape){
+    matrix::matrix(const std::vector<float>& data,
+            const std::vector<size_t>& shape)
+    {
         m_matrix_ptr = std::make_unique<processor::matrix_object>(data, shape);
         m_matrix_id = matrix_id++;
     }
@@ -21,6 +23,13 @@ namespace TakiMatrix {
     {
         m_matrix_ptr = std::move(new_matrix.m_matrix_ptr);
         m_matrix_id = matrix_id++;
+    }
+
+    size_t matrix::get_id() const { return m_matrix_id; }
+
+    size_t matrix_hash_functor::operator()(const matrix& obj) const
+    {
+        return obj.get_id();
     }
 
 } // namespace TakiMatrix
