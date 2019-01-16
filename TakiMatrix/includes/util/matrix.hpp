1739a98eea4c2888161f834ebc5a287afae307f4
jwkim98 <jwkim98@kaist.ac.kr>
2019-01-16 17:13:25 +0900

    add: destructor for m_matrix_ptr constructor with matrix_object_ptr

--- TakiMatrix/includes/util/matrix.hpp
+++ TakiMatrix/includes/util/matrix.hpp
@@ -6,15 +6,23 @@
 #define TAKIMATRIX_MATRIX_HPP
 
 #include "../cpu_emulator/processor_util/matrix_object.hpp"
+#include "../cpu_emulator/system_agent/system_agent.hpp"
+#include "../cpu_emulator/processor_util/instruction_set.hpp"
+#include <condition_variable>
 #include <memory>
+
+using namespace TakiMatrix::processor;
 
 namespace TakiMatrix {
 
     size_t matrix_id = 0;
 
+
     class matrix {
     public:
         matrix(const std::vector<float>& data, const std::vector<size_t>& shape);
+
+        explicit matrix(matrix_object* matrix_object_ptr);
 
         matrix(matrix& new_matrix);
 
@@ -22,19 +30,34 @@ namespace TakiMatrix {
 
         ~matrix();
 
+        /**
+         * @brief : decodes following operation to ISA and renames matrix_object value
+         * matrix_object's data is updated when corresponding operation is committed
+         * @param first : matrix to perform addition(must have same shape)
+         * @return : matrix as a result (they don't necessarily contain result after this operation)
+         */
+        matrix operator+(const matrix& first);
+
+        matrix operator-(const matrix& first);
+
+        matrix operator*(const matrix& first);
+        /**
+         * @brief : compares matrices element by element
+         * @param first : matrix to compare with
+         * @return : true if matrices are identical
+         */
+        bool operator==(const matrix& first);
+
+        bool operator!=(const matrix& first);
+        /// think about branches
         size_t get_id() const;
 
     private:
-        std::unique_ptr<processor::matrix_object> m_matrix_ptr;
+        void wait_until_enabled();
+
+        matrix_object* m_matrix_ptr;
 
         size_t m_matrix_id;
-    };
-
-    struct compare_matrix {
-        bool operator()(const matrix& first, const matrix& second)
-        {
-            return first.get_id()>second.get_id();
-        }
     };
 
     struct matrix_hash_functor {
--- TakiMatrix/sources/util/matrix.cpp
+++ TakiMatrix/sources/util/matrix.cpp
@@ -8,28 +8,60 @@ namespace TakiMatrix {
     matrix::matrix(const std::vector<float>& data,
             const std::vector<size_t>& shape)
     {
-        m_matrix_ptr = std::make_unique<processor::matrix_object>(data, shape);
+        m_matrix_ptr = new matrix_object(data, shape);
+        m_matrix_id = matrix_id++;
+    }
+
+    matrix::matrix(matrix_object* matrix_object_ptr)
+    {
+        m_matrix_ptr = matrix_object_ptr;
         m_matrix_id = matrix_id++;
     }
 
     matrix::matrix(matrix& new_matrix)
     {
         processor::matrix_object temp = *new_matrix.m_matrix_ptr;
-        m_matrix_ptr = std::make_unique<processor::matrix_object>(temp);
+        m_matrix_ptr = new matrix_object(temp);
         m_matrix_id = matrix_id++;
     }
 
     matrix::matrix(matrix&& new_matrix) noexcept
     {
-        m_matrix_ptr = std::move(new_matrix.m_matrix_ptr);
+        m_matrix_ptr = new_matrix.m_matrix_ptr;
         m_matrix_id = matrix_id++;
     }
 
+    matrix::~matrix(){
+        delete m_matrix_ptr;
+    }
+
     size_t matrix::get_id() const { return m_matrix_id; }
+
+    matrix matrix::operator+(const matrix& first)
+    {
+        std::vector<size_t> shape{m_matrix_ptr->get_shape().at(0),
+                                  first.m_matrix_ptr->get_shape().at(1), 0};
+        auto result_matrix_object_ptr = new matrix_object(shape);
+        auto instruction =
+                add(m_matrix_ptr, first.m_matrix_ptr, result_matrix_object_ptr);
+        auto lock = std::unique_lock(system_agent::m_fetch_schedule_mtx);
+        system_agent::fetch_enable.wait(lock);
+        return matrix(result_matrix_object_ptr);
+    }
+
+    matrix matrix::operator-(const matrix& first) { }
+
+    matrix matrix::operator*(const matrix& first) { }
+
+    bool matrix::operator==(const matrix& first) { }
+
+    bool matrix::operator!=(const matrix& first) { return !(*this==first); }
 
     size_t matrix_hash_functor::operator()(const matrix& obj) const
     {
         return obj.get_id();
     }
+
+    void matrix::wait_until_enabled() { }
 
 } // namespace TakiMatrix
