07cd25944095ed6cfcb54e1f751d57e629364428
jwkim98 <jwkim98@kaist.ac.kr>
2019-01-18 17:33:23 +0900

    refactor: matrix_object is deleted only when matrix is l-value

--- TakiMatrix/includes/util/matrix.hpp
+++ TakiMatrix/includes/util/matrix.hpp
@@ -5,9 +5,9 @@
 #ifndef TAKIMATRIX_MATRIX_HPP
 #define TAKIMATRIX_MATRIX_HPP
 
-#include "../cpu_emulator/processor_util/matrix_object.hpp"
-#include "../cpu_emulator/system_agent/system_agent.hpp"
 #include "../cpu_emulator/processor_util/instruction_set.hpp"
+#include "../cpu_emulator/processor_util/matrix_object.hpp"
+#include "../cpu_emulator/system_agent/process.hpp"
 #include <condition_variable>
 #include <memory>
 
@@ -17,12 +17,11 @@ namespace TakiMatrix {
 
     size_t matrix_id = 0;
 
-
     class matrix {
     public:
-        matrix(const std::vector<float>& data, const std::vector<size_t>& shape);
+        matrix(process& processor,const std::vector<float>& data, const std::vector<size_t>& shape);
 
-        explicit matrix(matrix_object* matrix_object_ptr);
+        explicit matrix(process& processor, matrix_object* matrix_object_ptr);
 
         matrix(matrix& new_matrix);
 
@@ -34,7 +33,8 @@ namespace TakiMatrix {
          * @brief : decodes following operation to ISA and renames matrix_object value
          * matrix_object's data is updated when corresponding operation is committed
          * @param first : matrix to perform addition(must have same shape)
-         * @return : matrix as a result (they don't necessarily contain result after this operation)
+         * @return : matrix as a result (they don't necessarily contain result after
+         * this operation)
          */
         matrix operator+(const matrix& first);
 
@@ -43,21 +43,25 @@ namespace TakiMatrix {
         matrix operator*(const matrix& first);
         /**
          * @brief : compares matrices element by element
+         * these instructions does not create isa
          * @param first : matrix to compare with
          * @return : true if matrices are identical
          */
         bool operator==(const matrix& first);
 
         bool operator!=(const matrix& first);
+
         /// think about branches
         size_t get_id() const;
 
     private:
-        void wait_until_enabled();
-
+        /// pointer to matrix_object
+        /// deleted by destructor only when matrix is l-value reference
         matrix_object* m_matrix_ptr;
 
-        size_t m_matrix_id;
+        size_t m_matrix_id;;
+
+        std::reference_wrapper<process> m_processor;
     };
 
     struct matrix_hash_functor {
--- TakiMatrix/sources/util/matrix.cpp
+++ TakiMatrix/sources/util/matrix.cpp
@@ -5,20 +5,23 @@
 #include "../../includes/util/matrix.hpp"
 
 namespace TakiMatrix {
-    matrix::matrix(const std::vector<float>& data,
+    matrix::matrix(process& processor, const std::vector<float>& data,
             const std::vector<size_t>& shape)
+            :m_processor(processor)
     {
-        m_matrix_ptr = new matrix_object(data, shape);
         m_matrix_id = matrix_id++;
+        m_matrix_ptr = new matrix_object(data, shape, true);
     }
 
-    matrix::matrix(matrix_object* matrix_object_ptr)
+    matrix::matrix(process& processor, matrix_object* matrix_object_ptr)
+            :m_processor(processor)
     {
         m_matrix_ptr = matrix_object_ptr;
         m_matrix_id = matrix_id++;
     }
 
     matrix::matrix(matrix& new_matrix)
+            :m_processor(new_matrix.m_processor)
     {
         processor::matrix_object temp = *new_matrix.m_matrix_ptr;
         m_matrix_ptr = new matrix_object(temp);
@@ -26,13 +29,16 @@ namespace TakiMatrix {
     }
 
     matrix::matrix(matrix&& new_matrix) noexcept
+            :m_processor(new_matrix.m_processor)
     {
         m_matrix_ptr = new_matrix.m_matrix_ptr;
         m_matrix_id = matrix_id++;
     }
 
-    matrix::~matrix(){
-        delete m_matrix_ptr;
+    matrix::~matrix()
+    {
+        if (m_matrix_ptr->has_origin())
+            delete m_matrix_ptr;
     }
 
     size_t matrix::get_id() const { return m_matrix_id; }
@@ -44,16 +50,37 @@ namespace TakiMatrix {
         auto result_matrix_object_ptr = new matrix_object(shape);
         auto instruction =
                 add(m_matrix_ptr, first.m_matrix_ptr, result_matrix_object_ptr);
-        auto lock = std::unique_lock<std::mutex>(system_agent::m_fetch_schedule_mtx);
-        system_agent::fetch_enable.wait(lock);
-        return matrix(result_matrix_object_ptr);
+        m_processor.get().instruction_queue_push(instruction);
+        return matrix(m_processor, result_matrix_object_ptr);
     }
 
-    matrix matrix::operator-(const matrix& first) { }
+    matrix matrix::operator-(const matrix& first)
+    {
+        std::vector<size_t> shape{m_matrix_ptr->get_shape().at(0),
+                                  first.m_matrix_ptr->get_shape().at(1), 0};
+        auto result_matrix_object_ptr = new matrix_object(shape);
+        auto instruction =
+                sub(m_matrix_ptr, first.m_matrix_ptr, result_matrix_object_ptr);
+        m_processor.get().instruction_queue_push(instruction);
+        return matrix(m_processor, result_matrix_object_ptr);
+    }
 
-    matrix matrix::operator*(const matrix& first) { }
+    matrix matrix::operator*(const matrix& first)
+    {
+        std::vector<size_t> shape{m_matrix_ptr->get_shape().at(0),
+                                  first.m_matrix_ptr->get_shape().at(1), 0};
+        auto result_matrix_object_ptr = new matrix_object(shape);
+        auto instruction =
+                mul(m_matrix_ptr, first.m_matrix_ptr, result_matrix_object_ptr);
+        m_processor.get().instruction_queue_push(instruction);
+        return matrix(m_processor, result_matrix_object_ptr);
+    }
 
-    bool matrix::operator==(const matrix& first) { }
+    bool matrix::operator==(const matrix& first)
+    {
+        m_processor.get().instruction_queue_wait_until_empty();
+        return *(first.m_matrix_ptr)==*m_matrix_ptr;
+    }
 
     bool matrix::operator!=(const matrix& first) { return !(*this==first); }
 
@@ -61,7 +88,5 @@ namespace TakiMatrix {
     {
         return obj.get_id();
     }
-
-    void matrix::wait_until_enabled() { }
 
 } // namespace TakiMatrix
