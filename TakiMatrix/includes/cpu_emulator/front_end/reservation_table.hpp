fc01beb2dbfa1bf14583389e60a89a51682e75a9
jwkim98 <jwkim98@kaist.ac.kr>
2019-01-18 17:31:31 +0900

    add: reservation_table

--- TakiMatrix/includes/cpu_emulator/front_end/reservation_table.hpp
+++ TakiMatrix/includes/cpu_emulator/front_end/reservation_table.hpp
@@ -5,7 +5,39 @@
 #ifndef TAKIMATRIX_RESERVATION_TABLE_HPP
 #define TAKIMATRIX_RESERVATION_TABLE_HPP
 
-namespace TakiMatrix::processor{
+#include "../../cpu_emulator/processor_util/matrix_object.hpp"
+#include "../processor_util/instruction_set.hpp"
+#include <condition_variable>
+#include <deque>
+#include <list>
+#include <thread>
+#include <unordered_set>
 
-}
-#endif //TAKIMATRIX_RESERVATION_TABLE_HPP
+namespace TakiMatrix::processor {
+    class reservation_table {
+    public:
+        /**
+         * implements reservation table that can be concurrently accessed
+         * @param table_size : maximum size of the table
+         */
+        explicit reservation_table(size_t table_size = 100);
+        /**
+         * inserts instruction to the end of the reservation table
+         * @param instruction : instruction to insert
+         */
+        void insert(const isa& instruction);
+        /**
+         * scans the reservation table, and puts executable instructions to
+         * start_list (collects instructions without read-after-write dependency)
+         * @param start_list : reference of deque to insert executable instructions
+         */
+        void scan(std::deque<isa>& start_list);
+
+    private:
+        const size_t m_maximum_table_size;
+        std::list<isa> m_reservation_table;
+        std::mutex m_mtx;
+        std::condition_variable m_cond;
+    };
+} // namespace TakiMatrix::processor
+#endif // TAKIMATRIX_RESERVATION_TABLE_HPP
--- TakiMatrix/sources/cpu_emulator/front_end/reservation_table.cpp
+++ TakiMatrix/sources/cpu_emulator/front_end/reservation_table.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "../../../includes/cpu_emulator/front_end/reservation_table.hpp"
+
+namespace TakiMatrix::processor {
+
+    reservation_table::reservation_table(size_t table_size)
+            :m_maximum_table_size(table_size) { }
+
+    void reservation_table::insert(const isa& instruction)
+    {
+        std::unique_lock<std::mutex> lock(m_mtx);
+        m_cond.wait(lock, [this]() {
+            return m_reservation_table.size()<m_maximum_table_size;
+        });
+        m_reservation_table.emplace_back(instruction);
+        lock.unlock();
+        m_cond.notify_all();
+    }
+
+    void reservation_table::scan(std::deque<isa>& start_list)
+    {
+        std::unordered_set<size_t> write_operated_matrix_object_ids;
+
+        auto has_previous_write_operation =
+                [&write_operated_matrix_object_ids](size_t matrix_object_id) {
+                    if (write_operated_matrix_object_ids.find(matrix_object_id)==
+                            write_operated_matrix_object_ids.end())
+                        return false;
+                    return true;
+                };
+
+        std::unique_lock<std::mutex> lock(m_mtx);
+        m_cond.wait(lock, [this]() { return !m_reservation_table.empty(); });
+        auto rs_table_itr = m_reservation_table.begin();
+        for (; rs_table_itr!=m_reservation_table.end(); rs_table_itr++) {
+            size_t matrix_object_id = rs_table_itr->get_result_ptr()->get_id();
+            if (!has_previous_write_operation(matrix_object_id)) {
+                start_list.emplace_back(*rs_table_itr);
+                rs_table_itr = m_reservation_table.erase(rs_table_itr);
+                rs_table_itr--;
+            }
+        }
+        lock.unlock();
+        m_cond.notify_all();
+    }
+
+} // namespace TakiMatrix::processor
