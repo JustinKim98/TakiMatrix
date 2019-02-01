e38ae6308ba121841e93d712345ced65039ff2c5
jwkim98 <jwkim98@kaist.ac.kr>
2019-02-01 18:40:09 +0900

    add: getters for first, second operand ptrs

--- TakiMatrix/sources/cpu_emulator/processor_util/instruction_set.cpp
+++ TakiMatrix/sources/cpu_emulator/processor_util/instruction_set.cpp
@@ -1,5 +1,5 @@
 /**
- * instruction_set.cpp
+ * @file : instruction_set.cpp
  */
 
 #include "../../../includes/cpu_emulator/processor_util/instruction_set.hpp"
@@ -20,6 +20,10 @@ namespace TakiMatrix::processor {
             :m_instruction_type(type), m_operand_first(operand_first),
              m_operand_second(operand_second), m_result(result), m_functor(functor) { }
 
-    std::shared_ptr<matrix_object> instruction::get_result_ptr() { return m_result; }
+    std::shared_ptr<matrix_object> instruction::result_ptr() { return m_result; }
+
+    std::shared_ptr<matrix_object> instruction::first_operand_ptr() { return m_operand_first; }
+
+    std::shared_ptr<matrix_object> instruction::second_operand_ptr() { return m_operand_second; }
 
 } // namespace TakiMatrix::processor
